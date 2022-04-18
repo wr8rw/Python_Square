f11918ba60988fbfa65e2d1b303ff34492157b23
wr8rw <103325414+wr8rw@users.noreply.github.com>
2022-04-18 09:55:27 -0400

    Add files via upload

---
+++ Python_Square.py
@@ -0,0 +1,15 @@
+import turtle
+rsw = turtle.Turtle()
+rsw.shape('turtle')
+
+rsw.forward(100)
+rsw.right(90)
+
+rsw.forward(100)
+rsw.right(90)
+
+rsw.forward(100)
+rsw.right(90)
+
+rsw.forward(100)
+rsw.right(90)
